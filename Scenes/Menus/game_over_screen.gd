ede644c176d55313bfd5ab596833cafc92e66210
Martijn Tielemans <martijn.tielemans@live.nl>
2024-10-07 20:44:22 +0200

    Added ranking illustrations

--- Scenes/Menus/game_over_screen.gd
+++ Scenes/Menus/game_over_screen.gd
@@ -8,10 +8,12 @@ var canCountScore : bool = true
 @export var playerScoreNumber : Label
 @export var highScoreNumber : Label
 @export var rankImage : TextureRect
+@export var rankIllust : TextureRect
 @export var bigText : TextureRect
 @export var winText : CompressedTexture2D
 
 @export var rankImages : Array[CompressedTexture2D]
+@export var rankIllustrations : Array[CompressedTexture2D]
 @export var rankSongs : Array[String]
 @export var rankThresholds : Array[int]
 
