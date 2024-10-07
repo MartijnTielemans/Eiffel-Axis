4bd0936764542c8ab4b18d5fca3fcedf604ee935
Martijn Tielemans <martijn.tielemans@live.nl>
2024-10-07 02:00:22 +0200

    Finished Boss functionality
    - Added player win conditions and transition

--- Scripts/damage_player.gd
+++ Scripts/damage_player.gd
@@ -5,6 +5,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player and not parent.player_character == null:
 		parent.player_character.update_health(-1)
-	else:
+	elif body is Player:
 		var player = get_node("/root/Level/PlayerCharacter")
 		player.update_health(-1)
